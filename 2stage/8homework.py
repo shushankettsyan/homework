29777ee03aa1190f776abadfdd005f77888ebde4
shushan <shushankettsyan@gmail.com>
2020-10-24 11:48:16 +0400

    ...

---
+++ 2stage/8homework.py
@@ -0,0 +1,40 @@
+import json
+import yaml
+
+#-------------------------------------------------------------------------------
+# json to text
+# with open("json.json") as js:
+#     data = json.load(js)
+#
+# with open("test.txt", "w") as txt:
+#     json.dump(data, txt)
+
+
+
+#-------------------------------------------------------------------------------
+#json to yaml
+# with open("json.json") as js:
+#     data = json.load(js)
+#
+# with open("test.yaml", "w") as yml:
+#     yaml.dump(data, yml)
+
+
+
+#-------------------------------------------------------------------------------
+#yaml to json
+# with open("test.yaml") as yml:
+#     data = yaml.load(yml)
+#
+# with open("test.json", "w") as js:
+#     json.dump(data, js)
+
+
+
+#-------------------------------------------------------------------------------
+#yaml to txt
+# with open("test.yaml") as yml:
+#     data = yaml.load(yml)
+#
+# with open("test_1.txt", "w") as txt:
+#     yaml.write(data, txt)
